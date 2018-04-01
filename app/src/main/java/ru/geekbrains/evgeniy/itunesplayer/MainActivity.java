352dfbd15b5f65b9cd7b67c0e868299b05c8553f
Johni134 <kec@inbox.ru>
2018-04-01 07:49:00 +0700

    +CompositeDisposable (disposing ondestroy)

--- app/src/main/java/ru/geekbrains/evgeniy/itunesplayer/MainActivity.java
+++ app/src/main/java/ru/geekbrains/evgeniy/itunesplayer/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.Toast;
 
 import com.jakewharton.retrofit2.adapter.rxjava2.HttpException;
 
-import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
 import java.io.IOException;
@@ -25,14 +24,15 @@ import java.util.concurrent.RecursiveAction;
 
 import io.reactivex.FlowableSubscriber;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subscribers.DisposableSubscriber;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String ITUNES_MEDIA_TYPE = "music";
-    private Disposable disposable = null;
+    private static final CompositeDisposable compositeDisposable = new CompositeDisposable();
     RecyclerView recyclerView;
     List<ModelResponceResult> modelResponceResultList;
     EditText editTextKeyword;
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         buttonResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String keywords = editTextKeyword.getText().toString();
 
                 if(keywords.trim().length() < 5) {
@@ -65,30 +66,28 @@ public class MainActivity extends AppCompatActivity {
                             getString(R.string.error_5_min_characters), Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-                tryToUnsubscribe();
-
                 try {
-                    disposable = App.getApi()
+                    compositeDisposable.add(App.getApi()
                             .getData(URLEncoder.encode(keywords, "UTF-8"), ITUNES_MEDIA_TYPE)
                             .subscribeOn(Schedulers.io())
                             .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe(new Consumer<ModelResponse>() {
+                            .subscribeWith(new DisposableSubscriber<ModelResponse>() {
+
                                 @Override
-                                public void accept(ModelResponse modelResponse) throws Exception {
+                                public void onNext(ModelResponse modelResponse) {
                                     modelResponceResultList.clear();
                                     modelResponceResultList.addAll(modelResponse.getResults());
                                     recyclerView.scrollToPosition(0);
                                     recyclerView.getAdapter().notifyDataSetChanged();
                                 }
-                            }, new Consumer<Throwable>() {
+
                                 @Override
-                                public void accept(Throwable throwable) throws Exception {
-                                    if (throwable instanceof IOException) {
+                                public void onError(Throwable t) {
+                                    if (t instanceof IOException) {
                                         Toast.makeText(MainActivity.this,
                                                 getString(R.string.check_internet_connection),
                                                 Toast.LENGTH_SHORT).show();
-                                    } else if (throwable instanceof HttpException) {
+                                    } else if (t instanceof HttpException) {
                                         Toast.makeText(MainActivity.this,
                                                 getString(R.string.server_error),
                                                 Toast.LENGTH_SHORT).show();
@@ -98,7 +97,12 @@ public class MainActivity extends AppCompatActivity {
                                                 Toast.LENGTH_SHORT).show();
                                     }
                                 }
-                            });
+
+                                @Override
+                                public void onComplete() {
+
+                                }
+                            }));
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
@@ -114,14 +118,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void tryToUnsubscribe() {
-        if (disposable != null && !disposable.isDisposed())
-            disposable.dispose();
-    }
-
     @Override
     protected void onDestroy() {
-        tryToUnsubscribe();
+        compositeDisposable.dispose();
         super.onDestroy();
     }
 }
